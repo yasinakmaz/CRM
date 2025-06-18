938125189005099a9abf09ffd3a2e18555e2e517
Yasin Akmaz <akmazkedy@gmail.com>
2025-06-18 17:40:02 +0300

    Add project files.

---
+++ CRM/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿namespace CRM
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .UseMauiCommunityToolkitCore()
+                .ConfigureSyncfusionCore()
+                .ConfigureSyncfusionToolkit()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("EthosNova-Bold.ttf", "EthosNovaBold");
+                    fonts.AddFont("EthosNova-Medium.ttf", "EthosNovaMedium");
+                    fonts.AddFont("EthosNova-Heavy.ttf", "EthosNovaHeavy");
+                    fonts.AddFont("EthosNova-Regular.ttf", "EthosNovaRegular");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
